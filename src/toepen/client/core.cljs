8518f9d2f7ee5511affa7c880b7d51e7e2d5b824
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-04-02 21:05:25 +0200

    Working on websocket connection

--- src/toepen/client/core.cljs
+++ src/toepen/client/core.cljs
@@ -1,1 +1,16 @@
-(ns toepen.client.core)
+(ns toepen.client.core
+  (:require-macros [cljs.core.async.macros :as asyncm :refer (go go-loop)])
+  (:require [cljs.core.async :as async :refer (<! >! put! chan)]
+            [taoensso.sente  :as sente :refer (cb-success?)]))
+
+(def channel (sente/make-channel-socket! "/chsk" {:type :auto}))
+
+(comment
+  (let [{:keys [chsk ch-recv send-fn state]}]
+
+
+    (def chsk       chsk)
+    (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
+    (def chsk-send! send-fn) ; ChannelSocket's send API fn
+    (def chsk-state state)))   ; Watchable, read-only atom
+
--- src/toepen/server/core.clj
+++ src/toepen/server/core.clj
@@ -1,8 +1,20 @@
 (ns toepen.server.core
   (:require [org.httpkit.server :as http]
-            [reitit.ring :as ring]))
+            [reitit.ring :as ring]
+            [ring.util.response :as resp]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [taoensso.sente :as sente]
+            [taoensso.sente.server-adapters.http-kit :refer (get-sch-adapter)]))
+
+; TODO Add anti-forgery-middleware
+; TODO Tweak the middlware until it works
+
 
 (defonce server (atom nil))
+(def channel (sente/make-channel-socket! (get-sch-adapter) {}))
+
+(def connected-uids (:connected-uids channel))
 
 (defn stop-server
   []
@@ -12,17 +24,34 @@
     (@server :timeout 100)
     (reset! server nil)))
 
+(defn index
+  [_]
+  (resp/resource-response "index.html" {:root "public"}))
+
 (def handler
   (ring/ring-handler
     (ring/router
-     ["/ping" (constantly {:status 200 :body "pong"})])
+     [["/chsk" {:get {:middleware [[wrap-keyword-params] [wrap-params]]
+                      :handler (:ajax-get-or-ws-handshake-fn channel)}
+                :post {:middleware [[wrap-keyword-params] [wrap-params]]
+                       :handler (:ajax-post-fn channel)}}]
+      ["/" index]
+      ["/:game" index]]
+     {:conflicts nil})
     (ring/routes
       (ring/create-resource-handler {:path "/"})
       (ring/create-default-handler))))
 
+(handler {:request-method :get :uri "/chsk?udt=1585853173870&client-id=06aba6cd-58f0-4632-b91e-e2525c09508c&handshake?=true"})
+
+(handler {:request-method :get :uri "/ping"})
+
+
 (comment
-  (reset! server (http/run-server #'handler {:port 8080}))
-  (stop-server)
+  (do
+    (stop-server)
+    (reset! server (http/run-server #'handler {:port 8080})))
+
   nil)
 
 (defn -main
