d8efd9a460876f47596e9c1294e9d98cea472e11
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-04-21 20:58:41 +0200

    Adds button action to front page

--- src/toepen/client/core.cljs
+++ src/toepen/client/core.cljs
@@ -1,18 +1,25 @@
 (ns toepen.client.core
   (:require [reagent.dom :as rdom]
+            [reagent.core :as r]
             [toepen.client.ui :as ui]
             [toepen.client.state :as state]))
 
 (defn index
   []
-  [:div {:class "w-screen h-screen flex flex-row items-center justify-center"}
-   [:div {:class "w-1/2 flex flex-col items-center justify-start"}
-    [:img {:src "logo.jpg"}]
-    [:input {:class "mt-8 text-xl focus:outline-none focus:shadow-outline border-2 border-blue-400 py-2 px-4 block appearance-none leading-normal"
-             :style {:width "22rem"}
-             :placeholder "ProudPinoPrancingVictoriously"}]
-    [:button {:class "mt-4 text-xl text-blue-100 bg-blue-400 hover:bg-blue-600 font-bold py-2 px-8"}
-     "Toep!"]]])
+  (let [state (r/atom {:proposed-game "ProudPinoPrancingVictoriously"
+                       :proposed-game-typed "ProudPinoPrancingVictoriously"
+                       :game "ProudPinoPrancingVictoriously"})
+        start-game (fn [] (set! (.. js/window -location -href) (str "/" (get @state :game))))]
+    (fn []
+      [:div {:class "w-screen h-screen flex flex-row items-center justify-center"}
+       [:div {:class "w-1/2 flex flex-col items-center justify-start"}
+        [:img {:src "logo.jpg"}]
+        [:input {:class "mt-8 text-xl focus:outline-none focus:shadow-outline border-2 border-blue-400 py-2 px-4 block appearance-none leading-normal"
+                 :style {:width "22rem"}
+                 :placeholder (get @state :proposed-game-typed "")}]
+        [:button {:class "mt-4 text-xl text-blue-100 bg-blue-400 hover:bg-blue-600 font-bold py-2 px-8"
+                  :on-click start-game}
+         "Toep!"]]])))
 
 (let [el (js/document.getElementById "app")
       data (.getAttribute el "data")]
