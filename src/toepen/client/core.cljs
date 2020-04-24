403cf8d58fbf989868a5af2426e044b6fdafd3cd
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-04-24 09:50:24 +0200

    Adds automatic haiku names

--- src/toepen/client/core.cljs
+++ src/toepen/client/core.cljs
@@ -2,21 +2,59 @@
   (:require [reagent.dom :as rdom]
             [reagent.core :as r]
             [toepen.client.ui :as ui]
-            [toepen.client.state :as state]))
+            [toepen.client.state :as state]
+            [toepen.common.haiku-names :refer [haiku]]
+            [clojure.string :as str]
+            [cljs.core.async :refer [<! timeout]])
+  (:require-macros [cljs.core.async.macros :as m :refer [go-loop]]))
+
+(defn new-name
+  [state]
+  (let [nn (haiku)]
+    (-> state
+        (assoc :proposed-game nn)
+        (assoc :proposed-game-typed "")
+        (assoc :type-queue (into [] nn)))))
+
+(defn type-letter
+  [{:keys [type-queue] :as state}]
+  (-> state
+      (update :proposed-game-typed str (first type-queue))
+      (update :type-queue rest)))
 
 (defn index
   []
-  (let [state (r/atom {:proposed-game "ProudPinoPrancingVictoriously"
-                       :proposed-game-typed "ProudPinoPrancingVictoriously"
-                       :game "ProudPinoPrancingVictoriously"})
-        start-game (fn [] (set! (.. js/window -location -href) (str "/" (get @state :game))))]
+  (let [state (r/atom {:proposed-game ""
+                       :proposed-game-typed ""
+                       :type-queue []
+                       :game ""})
+        start-game (fn [] (let [{:keys [game proposed-game]} @state
+                                dest (if (str/blank? game) proposed-game game)]
+                            (set! (.. js/window -location -href) (str "/" dest))))]
+
+    (go-loop []
+      (if (empty? (get @state :type-queue))
+        (do (<! (timeout 2500))
+            (swap! state new-name))
+        (do (<! (timeout 75))
+            (swap! state type-letter)))
+      (recur))
+
+    (swap! state new-name)
     (fn []
       [:div {:class "w-screen h-screen flex flex-row items-center justify-center"}
        [:div {:class "w-1/2 flex flex-col items-center justify-start"}
         [:img {:src "logo.jpg"}]
-        [:input {:class "mt-8 text-xl focus:outline-none focus:shadow-outline border-2 border-blue-400 text-blue-400 py-2 px-4 block appearance-none leading-normal"
-                 :style {:width "22rem"}
-                 :placeholder (get @state :proposed-game-typed "")}]
+        [:input {:id "game-input"
+                 :class "mt-8 text-xl focus:outline-none focus:shadow-outline border-2 border-blue-400 text-blue-400 py-2 px-4 block appearance-none leading-normal"
+                 :style {:width "24rem"}
+                 :placeholder (get @state :proposed-game-typed "")
+                 :value (get @state :game)
+                 :on-change #(swap! state assoc :game (-> % .-target .-value))
+                 :on-key-down #(case (.-which %)
+                                 13 (start-game)
+                                 27 (swap! state assoc :game "")
+                                 nil)}]
         [:button {:class "mt-4 text-xl text-blue-100 bg-blue-400 hover:bg-blue-600 font-bold py-2 px-8"
                   :on-click start-game}
          "Toep!"]]])))
@@ -24,6 +62,7 @@
 (let [el (js/document.getElementById "app")
       data (.getAttribute el "data")]
   (case data
-    "index" (rdom/render [index] el)
+    "index" (do (rdom/render [index] el)
+                (.focus (js/document.getElementById "game-input")))
     "game" (do (rdom/render [ui/game] el)
                (state/refresh-state))))
--- src/toepen/common/haiku_names.cljc
+++ src/toepen/common/haiku_names.cljc
@@ -1,10 +1,41 @@
-(ns toepen.haiku-names)
+(ns toepen.common.haiku-names
+  (:require [clojure.string :as str]))
 
 (def adjectives
-  ["happy", "sad", "epic", "glorious" "proud"])
+  ["happy" "sad" "epic" "glorious" "proud" "generous" "aged" "ancient"
+   "billowing" "bitter" "broken" "calm" "cool" "crimson" "curly" "damp"
+   "dark" "delicate" "divine" "falling" "fancy" "fragrant" "frosty"
+   "gentle" "hidden" "holy" "jolly" "lingering" "little" "lively" "lucky"
+   "misty" "muddy" "mute" "nameless" "noisy" "odd" "patient" "plain"
+   "polished" "quiet" "rapid" "restless" "rough" "royal" "shiny" "shy"
+   "solitary" "sparkling" "chaotic" "sweet" "tiny" "wandering"
+   "weathered" "white" "wild" "wispy" "withered"])
 
 (def nouns
-  ["pino", "paula", "cards"])
+  ["pino" "paula" "tori" "pos" "amos" "jos" "voorzitter" "secretaris"
+   "penningmeester" "vicevoorzitter" "manager" "scientist" "botanist"
+   "biologist" "lecturer" "professor" "student"])
 
 (def verbs
-  ["prancing"])
+  ["prances" "toeps" "walks" "runs" "creates" "destroys" "eats" "draws"
+   "codes" "drinks" "smiles" "laughs" "sings" "waves" "surfs" "accepts"
+   "talks" "aims" "announces" "approves" "attempts" "becomes" "breaks"
+   "builds" "bows" "calculates" "communicates" "claims" "complains"
+   "dances" "desires" "deals" "disagrees" "explains" "experiments"
+   "follows" "hesitates" "investigates" "jumps" "kneels" "looks"
+   "manages" "negotiates" "overcomes" "postpones" "prevents" "prepares"
+   "protects" "performes" "recommends" "relaxes" "remembers" "sanctions"
+   "snores" "struggles" "survives" "threatens" "tries" "waits" "wins"])
+
+(def adjectives2
+  ["ably" "victoriously" "aggresively" "abashedly" "happily" "proudly"
+   "gloriously" "extraordinarily" "necessarily" "unhandily" "daintily"
+   "faultily" "prettily" "uneasily" "nastily" "sexily" "haughtily"
+   "unhappily" "grouchily"])
+
+(defn haiku
+  []
+  (->> [adjectives nouns verbs adjectives2]
+       (map rand-nth)
+       (map str/capitalize)
+       (str/join)))
