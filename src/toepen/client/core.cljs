af59b00aa22d46912b509cb22d2124ed62f524cd
Jelmer de Ronde <jelmerderonde@gmail.com>
2020-10-26 14:33:14 +0100

    Add toepboekje

--- src/toepen/client/core.cljs
+++ src/toepen/client/core.cljs
@@ -73,7 +73,16 @@
                                  nil)}]
         [:button {:class "mt-4 text-xl text-blue-100 bg-blue-400 hover:bg-blue-600 font-bold py-2 px-8"
                   :on-click start-game}
-         "Toep!"]]])))
+         "Toep!"]]
+       [:div {:class "absolute inset-x-0 top-0 bg-yellow-200 p-4 flex flex-row items-center justify-center"}
+        [:svg {:class "fill-current w-4 h-4 mr-2"
+               :xmlns "http://www.w3.org/2000/svg"
+               :viewBox "0 0 20 20"}
+          [:path {:d "M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"}]]
+        [:a {:class "font-mono"
+             :href "/toepboekje_5e_editie.pdf"
+             :target "_blank"}
+            "Download hier het Toepboekje"]]])))
 
 (start-pings)
 
